e281850c903a61a81005de78d41d3a4265637e15
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2008-11-09 17:47:31 +0000

    [Enhancement] Added Framework Logger Test File

---
+++ lib/framework/tests/class.Logger.test.php
@@ -0,0 +1,13 @@
+<?php
+class TestOfLoggerClass extends UnitTestCase {
+  function __contruct() {
+    $this->UnitTestCase('Logger Class Test');
+  }
+  
+  function setUp() {
+  }
+  
+  function tearDown() {
+  }
+}  
+?>
