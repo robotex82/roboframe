436656c9344bbc9b318ced6d42e078e085a127df
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2008-11-11 22:57:46 +0000

    [Fixed] Runner aborts on dot-files or folders (like ./.svn)

--- scripts/run_roboframe_tests.php
+++ scripts/run_roboframe_tests.php
@@ -16,7 +16,8 @@ class AllRoboframeTests extends TestSuite {
     if(is_dir($roboframe_tests_dir = FRAMEWORK_PATH.'/tests')) {
       if ($handle = opendir($roboframe_tests_dir)) {
         while (false !== ($roboframe_tests_dir_entry = readdir($handle))) {
-          if ($roboframe_tests_dir_entry != "." && $roboframe_tests_dir_entry != "..") {
+          //if ($roboframe_tests_dir_entry != "." && $roboframe_tests_dir_entry != "..") {
+          if (substr($roboframe_tests_dir_entry, 0, 1) != ".") {
             $this->addFile($roboframe_tests_dir.'/'.$roboframe_tests_dir_entry);
           }
         }
