118d3757d99c139a69203df15984ebe470aa26e6
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2008-11-13 16:41:08 +0000

    [Enhancement] Logger can be called statically now [Enhancement] mySQL Adapter throws exceptions if it can't connect

--- lib/framework/classes/class.MysqlAdapter.php
+++ lib/framework/classes/class.MysqlAdapter.php
@@ -1,12 +1,24 @@
 <?php
 class MysqlAdapter {
   public static function connect($settings) {
-    $connection = ADONewConnection($settings['adapter']);
-
-    if(!($connection->PConnect($settings['host'], $settings['username'], $settings['password']))) {
-        throw new Exception('Connection to database failed!');
+    
+/*
+    if(!($connection->PConnect($settings['host'], $settings['username'], $settings['password'], $settings['database']))) {
+      throw new Exception('Connection to database failed!');
     }
+*/
 
+    
+    try { 
+      $connection = ADONewConnection($settings['adapter']);
+      $connection->PConnect($settings['host'], $settings['username'], $settings['password'], $settings['database']);
+    } catch (exception $e) { 
+      echo 'Connection to database failed!';
+      var_dump($e->getMessage()); 
+      adodb_backtrace($e->gettrace());
+      die();
+    } 
+    
     return $connection;
   }
 }
