9ff3c0766e82334444c056c6c62bf0d0ee37e63f
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2008-11-13 14:48:48 +0000

    [Fixed] Controller name can't have more than one part

--- trunk/lib/framework/classes/class.Controller.php
+++ trunk/lib/framework/classes/class.Controller.php
@@ -22,12 +22,16 @@ abstract class Controller {
   
   public function forward($page, $action) {
     //e.g. HomeActions
-    $class = ucfirst($page) . "Controller";
+    $controller_without_underscore = str_replace('_', ' ', $page);
+    $controller_as_single_words = ucwords($controller_without_underscore);
+    $camelized_controller = str_replace(' ', '', $controller_as_single_words);
+    $class = $camelized_controller . "Controller";
+    //$class = ucwords($page) . "Controller";
     $file_class = $page . "_controller";
     //e.g. pages/home/HomeActions.php
     $file = CONTROLLER_ROOT . "/" . $file_class . ".php";
     if (!is_file($file)) {
-      exit("Controller [".CONTROLLER_ROOT . "/" . $class . ".php] not found");
+      exit("Controller [".$file."] not found");
     }
     require_once $file;
     $controller = new $class();
