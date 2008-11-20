4c799fe7a252c5444a6895e1ad4fa50906abf3e0
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2008-11-20 11:07:32 +0000

    [Enhancement] Logger takes an optional filename parameter. [Enhancement] Test coverage: First logger test implemented

--- lib/framework/tests/class.Logger.test.php
+++ lib/framework/tests/class.Logger.test.php
@@ -9,5 +9,14 @@ class TestOfLoggerClass extends UnitTestCase {
   
   function tearDown() {
   }
+  
+  function test_creates_file() {
+    $filename = dirname(__FILE__).'/../test_assets/Logger/test.log';  
+
+    @unlink($filename);
+    $this->assertFalse(file_exists($filename), 'Log file ['.$filename.'] should not exist!');    
+    Logger::write('Test should write this message to the logfile!', $filename);
+    $this->assertTrue(file_exists($filename), 'Log file ['.$filename.'] should exist after writing a message to the log');
+  }
 }  
 ?>
