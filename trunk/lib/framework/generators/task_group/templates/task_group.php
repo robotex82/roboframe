c8e86eae726b383edb0d6b26b8a9249d9a398f37
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2010-05-12 04:16:46 +0000

    [Fixed] TaskGroup Generator

---
+++ trunk/lib/framework/generators/task_group/templates/task_group.php
@@ -0,0 +1,10 @@
+<|?php
+class <?= $group_name ?>Tasks extends TaskGroup {
+  protected static $tasks = array('task' =>'prerequisite');
+  
+  /*
+  public function task($options) {
+    return true;
+  }
+  */
+}
