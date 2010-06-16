8b2f5c5aaabf845b4635168c93dbfbd5063f072c
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2010-06-16 08:41:39 +0000

    [Fixed] SVN Problems in TaskGroup Generator

---
+++ trunk/lib/framework/generators/controller/templates/task_group.php
@@ -0,0 +1,11 @@
+<|?php
+namespace TaskGroup;
+class <?= $group_name ?>Tasks extends Base {
+  protected static $tasks = array('task' =>'prerequisite');
+  
+  /*
+  public function task($options) {
+    return true;
+  }
+  */
+}
