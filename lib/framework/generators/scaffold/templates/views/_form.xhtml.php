d9f32145cd59f7661fbf10bb5e4bab5fbcf035b3
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2010-06-23 18:29:36 +0000

    [Change] Moved the scaffold generator to the active record plugin

--- lib/framework/generators/scaffold/templates/views/_form.xhtml.php
+++
@@ -1,5 +0,0 @@
-<|? foreach(<?= $model_class ?>::fields() as $field) : ?>
-  <|?= form_field_for(<?= $model_class ?>, $field, $<?= $us_model_class ?>); ?>
-<|? endforeach; ?>
-
-<p><input type="submit" value="submit"></input></p>
