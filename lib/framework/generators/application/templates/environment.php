de65dbff614655a36926b2f75befe8e367a4746c
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2010-08-20 12:10:17 +0000

    [Fixed] Environment is now read from Roboframe\Base::environment()

--- lib/framework/generators/application/templates/environment.php
+++ lib/framework/generators/application/templates/environment.php
@@ -4,6 +4,4 @@ Roboframe\Base::timezone("Europe/Berlin");
 Roboframe\Base::set_logger(new Logger\Text(APP_BASE.'/logs/'.\Roboframe\Base::environment().'_'.date("Y-m-d").'.log'));
 
 // include environment specific settings
-include(APP_BASE.'/config/environments/'.getenv('ROBOFRAME_ENV').'.php');
-
-?>
+include(APP_BASE.'/config/environments/'.\Roboframe\Base::environment().'.php');
