e931ca6013116768c514da9d59bf0f96d194a2f4
robotex82@arcor.de <robotex82@arcor.de@95ebc266-ac06-11dd-b655-b92e15b8dc27>
2010-10-19 12:47:06 +0000

    [Fixed] Changed generator templates to reflect actual api changes

--- lib/framework/generators/application/templates/dispatch.php
+++ lib/framework/generators/application/templates/dispatch.php
@@ -1,15 +1,5 @@
 <?php
 require_once('../config/bootstrap.php');
-
-define("VIEW_ROOT", APP_BASE . "/application/views");
-define("CONTROLLER_ROOT", APP_BASE . "/application/controllers");
-define("LAYOUT_ROOT", APP_BASE . "/application/layouts");
-
-// @TODO Move this to bootstrap.php
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . VIEW_ROOT);
-
 Roboframe\Base::enable_module('Controller\Front');
-//require_once FRAMEWORK_PATH.'/classes/class.FrontController.php';
-require_once CONTROLLER_ROOT.'/application_controller.php';
-
+require_once \Controller\Base::controller_root().'/application_controller.php';
 Controller\Front::createInstance()->dispatch();
